c1654179c4e33f3eda15d36bcf2ebd9ae99befa7
ralitsa.genova <146891262+raligen@users.noreply.github.com>
2024-08-03 01:57:44 +0300

    Add cards in Categories component

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,44 +1,87 @@
 export default function Categories() {
     return (
-        <>
-        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img className="d-block w-100" src="client\public\assets\img\katie-moum-politics.jpg" alt="Politics"/>
-            <div className="carousel-caption d-none d-md-block">
-                <h5>Politics</h5>
-                <p>Learn more about latest political developments.</p>
-            </div>   
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src="client\public\assets\img\john-schnobrich-business.jpg" alt="Business"/>
-            <div className="carousel-caption d-none d-md-block">
-                <h5>Business</h5>
-                <p>Click here for latest news and analyses on investment and entrepreneurship.</p>
-            </div>   
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src="/client/public/assets/img/alexander-shatov-technology.jpg." alt="Technology"/>
-            <div className="carousel-caption d-none d-md-block">
-                <h5>Technology</h5>
-                <p>Innovation never sleeps. Click here to learn more.</p>
-            </div>                
+        <div className="d-flex p-2 center">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+  
+        <div className="col c-item">
+          <div className="card h-100">
+            <img className="card-img-top c-img" src="https://images.unsplash.com/photo-1523292562811-8fa7962a78c8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Politics"/>
+            <div className="card-body">
+              <h5 className="card-title">Politics</h5>
+              <p className="card-text">
+                Learn more about latest political developments.
+              </p>
+            </div>
           </div>
         </div>
-        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
+  
+        <div className="col c-item">
+          <div className="card h-100">
+            <img className="card-img-top c-img" src="https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Business"/>
+            <div className="card-body">
+              <h5 className="card-title">Business</h5>
+              <p className="card-text">Click here for latest news and analyses on investment and entrepreneurship.</p>
+            </div>
+          </div>
+        </div>
+  
+        <div className="col c-item">
+          <div className="card h-100">
+            <img className="card-img-top c-img" src="https://images.unsplash.com/photo-1611264327630-8090373c8cef?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Technology"/>
+            <div className="card-body">
+              <h5 className="card-title">Technology</h5>
+              <p className="card-text">
+                Explore the latest news in technology.
+              </p>
+            </div>
+          </div>
+        </div>
+
       </div>
-      </>
+      </div>
     );
 }
+
+// export default function Categories() {
+//     return (
+//            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+//             <div className="carousel-indicators">
+//                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+//                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+//                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+//             </div>
+//             <div className="carousel-inner">
+//                 <div className="carousel-item active c-item">
+//                 <img className="d-block w-100 c-img" src="https://images.unsplash.com/photo-1523292562811-8fa7962a78c8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Politics"/>
+//                 <div className="carousel-caption d-none d-md-block">
+//                         <h5>Politics</h5>
+//                         <p>Learn more about latest political developments.</p>
+//                 </div>
+//                 </div>
+//                 <div className="carousel-item c-item">
+//                 <img className="d-block w-100 c-img" src="https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Business"/>
+//                 <div className="carousel-caption d-none d-md-block">
+//                         <h5>Business</h5>
+//                         <p>Click here for latest news and analyses on investment and entrepreneurship.</p>
+//                 </div>
+//                 </div>
+//                 <div className="carousel-item c-item">
+//                 <img className="d-block w-100 c-img" src="https://images.unsplash.com/photo-1611264327630-8090373c8cef?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Technology"/>
+//                 <div className="carousel-caption d-none d-md-block">
+//                         <h5>Technology</h5>
+//                         <p>Innovation never sleeps. Click here to learn more.</p>
+//                 </div>
+//                 </div>
+//             </div>
+//             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+//                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+//                 <span className="sr-only">Previous</span>
+//             </button>
+//             <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+//                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
+//                 <span className="sr-only">Next</span>
+//             </button>
+//             </div>
+//     );
+// }
+
